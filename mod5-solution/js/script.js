2a99df07426ee3815a7de7a27b1e13d4c4fdc93e
Matías Ignacio Castillo Pérez <matias.castillo@sansano.usm.cl>
2020-09-20 20:43:36 -0300

    mod5-solution2

--- mod5-solution/js/script.js
+++ mod5-solution/js/script.js
@@ -84,7 +84,7 @@ showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
   allCategoriesUrl,
   buildAndShowHomeHTML, // ***** <---- TODO: STEP 1: Substitute [...] ******
-  true); // Explicitly setting the flag to get JSON from server processed into an object literal
+  true); // Explicitely setting the flag to get JSON from server processed into an object literal
 });
 // *** finish **
 
@@ -118,11 +118,11 @@ function buildAndShowHomeHTML (categories) {
       var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "randomCategoryShortName", chosenCategoryShortName);
 
 
+
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
       insertHtml('#main-content', homeHtmlToInsertIntoMainPage);
-
     },
     false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
 }
